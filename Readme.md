8e36c98bb17e604df75ed9842f4529892836dccf
arman <theaiemperor@gmail.com>
2023-12-30 13:17:54 +0530

    Starting the development
    From here this Project Started.

---
+++ Readme.md
@@ -0,0 +1,46 @@
+<h3  align="center"> Go To Project : (https://armanai001.github.io/Simple-Text-Editor)</h3>
+
+###
+<h1>
+     
+</h1>
+
+###
+<h1 align="center">hey there 👋</h1>
+</br>
+
+<div align="center">
+  <a href="https://www.linkedin.com/in/arman-29001a259/" target="_blank">
+    <img src="https://raw.githubusercontent.com/maurodesouza/profile-readme-generator/master/src/assets/icons/social/linkedin/default.svg" width="76" height="40" alt="linkedin logo"  />
+  </a>
+  <a href="https://twitter.com/theaiemperor" target="_blank">
+    <img src="https://raw.githubusercontent.com/maurodesouza/profile-readme-generator/master/src/assets/icons/social/twitter/default.svg" width="76" height="40" alt="twitter logo"  />
+  </a>
+  <a href="https://discord.gg/Jt67zUCP" target="_blank">
+    <img src="https://raw.githubusercontent.com/maurodesouza/profile-readme-generator/master/src/assets/icons/social/discord/default.svg" width="76" height="40" alt="discord logo"  />
+  </a>
+</div>
+
+###
+
+<h3 align="left">👩‍💻  About Me</h3>
+
+###
+
+<p align="left">I'm Arman from India<br><br>- 🔭 I’m working as a frontend developer<br>- 📚 I'm currently learning plain HTML, CSS and JS<br>- ⚡ In my free time I created This simple text editor</p>
+
+###
+
+<h3 align="left">🛠 Language and tools</h3>
+
+###
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" height="40" alt="html5 logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" height="40" alt="css3 logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo"  />
+</div>
+
+###
---
+++ js/index.js
@@ -0,0 +1,150 @@
+const fontFamily = document.getElementById('FontSelect')
+const fontSize = document.getElementById('SizeSelect')
+const color = document.getElementById('ColorSelect')
+const backgroundColor = document.getElementById('backGroundSelect')
+const fontWeight = document.getElementById('boldButton')
+const fontStyle = document.getElementById('italicButton')
+const textDecoration = document.getElementById('underlineButton')
+const textArea = document.getElementById('textAreaInput')
+const leftAlign = document.getElementById('leftAlign')
+const centerAlign = document.getElementById('centerAlign')
+const rightAlign = document.getElementById('rightAlign')
+const letterSpacing = document.getElementById('letterSpaceSelect')
+const lineSpacing = document.getElementById('lineHeightSelect')
+const padding = document.getElementById('paddingSelect')
+
+
+const commonProperties = {
+    color: ['#000000', color],
+    backgroundColor: ['#ffffffff', backgroundColor],
+    fontWeight: ['normal', fontWeight],
+    fontStyle: ['normal', fontStyle],
+    textDecoration: ['none', textDecoration],
+    textAlign: ['left', leftAlign],
+    fontFamily: ["Arial", fontFamily],
+    fontSize: [15, fontSize],
+    letterSpacing: [1, letterSpacing],
+    lineHeight: [1, lineSpacing],
+    padding: [5, padding]
+}
+
+
+Object.keys(commonProperties).forEach(item => {
+    let getFromStorage = localStorage.getItem(item)
+    let propertyValue = getFromStorage !== null ? getFromStorage : commonProperties[item][0];
+
+
+    if (getFromStorage !== null && getFromStorage.includes('px')) {
+        propertyValue = parseInt(propertyValue.replace('px', ''))
+        textArea.style[item] = propertyValue + 'px';
+
+    } else {
+        textArea.style[item] = propertyValue;
+    }
+
+    if (item === 'padding') {
+        textArea.style[item] = propertyValue + 'px';
+    }
+
+    commonProperties[item][1].value = propertyValue;
+})
+
+
+textArea.innerText = localStorage.getItem('text') || "Start writting here..."
+
+
+
+const commonFonts = [
+    'Arial',
+    'Helvetica',
+    'Times New Roman',
+    'Times',
+    'Courier New',
+    'Courier',
+    'Verdana',
+    'Georgia',
+    'Palatino',
+    'Garamond',
+    'Bookman',
+    'Trebuchet MS',
+    'Arial Black',
+    'Impact',
+    'Comic Sans MS',
+    'Cursive',
+    'Arial Narrow',
+    'Century Gothic',
+    'Lucida Sans Unicode',
+    'Lucida Grande',
+    'Geneva',
+    'Monaco',
+    'Optima',
+    'Hoefler Text',
+    'Bitstream Vera Serif',
+    'Bitstream Vera Sans',
+    'Bitstream Vera Sans Mono',
+    'Candara',
+    'Constantia',
+    'Corbel',
+    'DejaVu Sans',
+    'DejaVu Serif',
+    'DejaVu Sans Mono',
+    'Lucida Console'
+];
+
+
+function listenEvents(propertyElement, property, value = null, additional = "") {
+
+    propertyElement.addEventListener(value === null ? "change" : 'click', (e) => {
+
+        if (value) {
+            textArea.style[property] = textArea.style[property] === "" ? value : "";
+            localStorage.setItem(property, textArea.style[property] === "" ? value : "");
+        } else {
+            textArea.style[property] = e.target.value + additional
+            localStorage.setItem(property, e.target.value + additional)
+        }
+
+    })
+}
+
+
+commonFonts.forEach(item => {
+    let newOption = document.createElement('option')
+    newOption.text = item
+    newOption.value = item
+    if (item === localStorage.getItem('fontFamily')) {
+        newOption.selected = true
+    }
+    fontFamily.appendChild(newOption)
+})
+
+
+listenEvents(fontFamily, 'fontFamily')
+listenEvents(fontSize, 'fontSize', null, 'px')
+listenEvents(letterSpacing, 'letterSpacing', null, 'px')
+listenEvents(padding, 'padding', null, 'px')
+listenEvents(lineSpacing, 'lineHeight', null, 'px')
+listenEvents(color, 'color')
+listenEvents(backgroundColor, 'backgroundColor')
+listenEvents(fontWeight, 'fontWeight', 'bold')
+listenEvents(fontStyle, 'fontStyle', 'italic')
+listenEvents(textDecoration, 'textDecoration', 'underline')
+listenEvents(leftAlign, 'textAlign', 'left')
+listenEvents(centerAlign, 'textAlign', 'center')
+listenEvents(rightAlign, 'textAlign', 'right')
+
+textArea.addEventListener("input",(e)=>{
+    if (textArea.innerText !== '') {
+    localStorage.setItem('text',textArea.innerText)
+    }else{
+        localStorage.removeItem('text')
+    }
+})
+
+let firstTime = true;
+textArea.addEventListener('click',(e)=>{
+    if (textArea.innerText === 'Start writting here...' && firstTime) {
+        textArea.innerText = "";
+        firstTime = false;
+    }
+})
